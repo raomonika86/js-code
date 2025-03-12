de3e337b984996061be09440ea94dda2f3efcaba
Monika Yadav <monikaydv8686@gmail.com>
2025-03-12 07:09:44 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3 + 3) // we are using nodejs, not browser
+
+let name = "monika"
+let age = 19
+let isloggedIn = false
+
+// premitiv datatypes
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standaloan value
+// undefined => 
+// symbol => unique
+
+// object datatype
+// 
+
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // null
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 123456
+// accountId = "123"  // not change const variable
+
+let accountEmail = "monika@gmail.com" 
+// accountEmail = "mona@gmail.com"  // change let variable
+
+var accountPassword = "123456789"
+// accountPassword = "gygu"  // change var variable
+
+accountCity = "Jaipur"
+// accountCity = "mumbai"
+
+
+console.log(accountCity, accountPassword, accountEmail);
+
+
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
