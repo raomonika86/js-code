3ca11a806f9530317ed9d2319af9b32264007c59
Monika Yadav <monikaydv8686@gmail.com>
2025-03-17 07:14:44 +0000

    connversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,30 @@
+// let score = null
+// let score = undefined
+// let score = 33
+// let score = "99"
+// let score = "33abc"
+// let score = true 
+let score = "monika" 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 33
+
+let changeNumber = String(someNumber)
+// console.log(changeNumber);
+console.log(typeof changeNumber);
+
+
